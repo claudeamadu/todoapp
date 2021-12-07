af4df60682de6323d198bab61f82e08f636827c4
Claude Amadu <obiriclaude@gmail.com>
2021-12-07 10:15:09 +0000

    create added

---
+++ lib/create_todo_view.dart
@@ -0,0 +1,88 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class CreateTodo extends StatelessWidget {
+  const CreateTodo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Create To Do"),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListView(
+            children: [
+              TextFormField(
+                initialValue: '',
+                decoration: const InputDecoration(
+                  labelText: 'Title',
+                  labelStyle: TextStyle(
+                    color: Color(0xFF6200EE),
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Color(0xFF6200EE)),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 100,
+              ),
+              TextFormField(
+                initialValue: '',
+                decoration: const InputDecoration(
+                  labelText: 'Description',
+                  labelStyle: TextStyle(
+                    color: Color(0xFF6200EE),
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Color(0xFF6200EE)),
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        initialValue: '',
+                        decoration: const InputDecoration(
+                          labelText: 'Date',
+                          labelStyle: TextStyle(
+                            color: Color(0xFF6200EE),
+                          ),
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Color(0xFF6200EE)),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        initialValue: '',
+                        decoration: const InputDecoration(
+                          labelText: 'Time',
+                          labelStyle: TextStyle(
+                            color: Color(0xFF6200EE),
+                          ),
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Color(0xFF6200EE)),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ));
+  }
+}
